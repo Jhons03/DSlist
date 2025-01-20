21543b5f0caf5990f7a4675d30b548fd2539ccc6
Edson Marco <edin2008@yahoo.com.br>
2025-01-20 20:24:26 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -18,13 +18,17 @@ public class Game {
 	private Long id;
 	private String title;
 	
-	@Column(name = "game_year")  // mudando o nome da coluna no bd
+	@Column(name = "game_year")
 	private Integer year;
 	private String  genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game () {
